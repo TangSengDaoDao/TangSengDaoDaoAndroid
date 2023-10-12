4bbcd7623127b9878d66d2b0201fecee1b94a7af
SL <No8blackball@163.com>
2023-10-12 11:04:59 +0800

    remove module build cached

--- wkuikit/build/intermediates/library_assets/debug/out/uikit_sql/202008101500.sql
+++
@@ -1,9 +0,0 @@
-create table prohibit_words
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sid int default 0,
-    content text,
-    `version` bigint default 0,
-    is_deleted smallint default 0,
-    created_at text
-)
