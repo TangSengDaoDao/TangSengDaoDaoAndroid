e59e537ba5ed583ba1c3bde04708765c884a234d
f001 <666666@qq.com>
2025-06-04 23:54:58 +0800

    fix：启动页全屏

--- app/src/main/java/com/mvc/bage/SplashActivity.kt
+++ app/src/main/java/com/mvc/bage/SplashActivity.kt
@@ -3,17 +3,13 @@ package com.mvc.bage
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.chat.base.WKBaseApplication
 import com.chat.base.config.WKSharedPreferencesUtil
@@ -36,20 +32,24 @@ public final class SplashActivity : AppCompatActivity() {
     val KEY_API_URL = "api_url"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window: Window = getWindow()
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.setStatusBarColor(Color.TRANSPARENT)
-
-
-            // 设置内容延伸到状态栏
-            window.getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            )
-        }
+        
+        // 确保全屏
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            val window: Window = getWindow()
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//            window.setStatusBarColor(Color.TRANSPARENT)
+//
+//
+//            // 设置内容延伸到状态栏
+//            window.getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+//                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//            )
+//        }
 
 
         setContentView(R.layout.splash_activity_with_animation)
